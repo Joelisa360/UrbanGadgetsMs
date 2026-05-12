31f6b9308ff6f85ff5897d2bbc508e4c89408eee
Joelisa360 <joelisa360@gmail.com>
2026-05-12 19:23:46 +0300

    temp use sql lite DB

--- Program.cs
+++ Program.cs
@@ -22,9 +22,11 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     });
 
 
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(
+//        builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite("Data Source=urbangadgets.db"));
 
 builder.Services.AddDistributedMemoryCache();
 
